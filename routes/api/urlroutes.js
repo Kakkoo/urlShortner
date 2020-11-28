8d215a99b040a2483af5c6fc8bd7d65633f9661a
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-11-28 11:12:11 -0800

    v

--- routes/api/urlroutes.js
+++ routes/api/urlroutes.js
@@ -10,35 +10,8 @@ router.get('/test', (req, res) => res.json({msg: 'urlroutes works'}));
 //@desc  take full url then give back short url
 //@access Public
 router.post('/full-shortURL', (req, res) => {
-  const FULLURL = req.body.fullurl;
-        const NUM = req.body.number;
-        const ALPHABET =
-          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        BASE = ALPHABET.length;
-        let digits = [];
-        while (NUM >= 1) {
-          let remainder = NUM % BASE;
-          digits.push(ALPHABET.charAt(remainder));
-          NUM = NUM / BASE;
-        }
-        let shorturl = digits.reverse().join("");
-        const newUrl = new Url({
-          fullurl: FULLURL,
-          number: NUM,
-          shorturl: shorturl,
-        });
-        newUrl
-          .save()
-          .then((Url) => res.json(Url))
-          .catch((err) => console.log("second last"));
-  // Url.find({ number: req.body.number })
-  //   .then((data) => {
-  //     if (!data) {
-  //       console.log(data);
-  //       return res.status(400).json({ number: "Number already exists." });
-  //     } else {
-  //       const FULLURL = req.body.fullurl;
-  //       const NUM = req.body.number;
+  // let FULLURL = req.body.fullurl;
+  //       let NUM = req.body.number;
   //       const ALPHABET =
   //         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   //       BASE = ALPHABET.length;
@@ -51,16 +24,43 @@ router.post('/full-shortURL', (req, res) => {
   //       let shorturl = digits.reverse().join("");
   //       const newUrl = new Url({
   //         fullurl: FULLURL,
-  //         number: NUM,
+  //         number: req.body.number;
   //         shorturl: shorturl,
   //       });
   //       newUrl
   //         .save()
   //         .then((Url) => res.json(Url))
   //         .catch((err) => console.log("second last"));
-  //     }
-  //   })
-  //   .catch((err) => console.log("last"));
+  Url.find({ number: req.body.number })
+    .then((data) => {
+      if (data) {
+        console.log(data);
+        return res.status(400).json({ number: "Number already exists." });
+      } else {
+        let FULLURL = req.body.fullurl;
+        let NUM = req.body.number;
+        const ALPHABET =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        BASE = ALPHABET.length;
+        let digits = [];
+        while (NUM >= 1) {
+          let remainder = NUM % BASE;
+          digits.push(ALPHABET.charAt(remainder));
+          NUM = NUM / BASE;
+        }
+        let shorturl = digits.reverse().join("");
+        const newUrl = new Url({
+          fullurl: FULLURL,
+          number: req.body.number,
+          shorturl: shorturl,
+        });
+        newUrl
+          .save()
+          .then((Url) => res.json(Url))
+          .catch((err) => console.log("second last"));
+      }
+    })
+    .catch((err) => console.log("last"));
 });
 
 module.exports = router;
