68c40058fddaf46114c8ecc33d7ea376baa6388e
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-11-29 08:27:57 -0800

    b

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
 //import jwt_decode from "jwt-decode";
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
 import setAuthToken from "./utils/setAuthToken";
-import Register from './components/auth/Register';
+import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import store from "./store";
+import { logoutUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
+import { SET_USER } from "./actions/types";
+import PrivateRoute from "./components/common/PrivateRoute";
+if (localStorage.jwtToken) {
+  //decode
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //check the expiry of the token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Expired
+    //Logout user
+    store.dispatch(logoutUser());
+    //Redirect user to login
+    window.location.href = "/login";
+  }
 
+  //Set auth header
   setAuthToken(localStorage.jwtToken);
+  //dispatch
+  store.dispatch({
+    type: SET_USER,
+    payload: decoded,
+  });
+}
 
-class App extends Component{
-  render(){
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component = {Landing} />
-        <Route exact path="/register" component = {Register} />
-        <Route exact path="/login" component = {Login} />
-        <Footer />
-      </div>
-    </Router>
-  );
-}}
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
 
 export default App;
