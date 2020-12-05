e5c303962625e98d6123f18d53a9362eb92029d8
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-12-04 18:52:12 -0800

    b

--- client/src/components/layout/Url.js
+++ client/src/components/layout/Url.js
@@ -1,34 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {giveshorturl} from "../../actions/authActions";
-const shorturl =  window.location.href;
-//const shorturl = Shorturl.slice(5);
-console.log(`clientshorturl + ${shorturl}`);
 class Url extends Component {
   constructor() {
     super();
     this.state = {
-     shorturl: shorturl,
-      
-      //shorturl: window.location.href
+     shorturl: this.props.location.pathname,
     };
-   // this.onload = this.onload.bind(this);
   }
-  // onload(e) {
-  //   e.preventDefault();
-  //   const shorturl = {
-  //     shorturl: this.state.shorturl 
-  //   };
-  //   this.props.giveshorturl(shorturl);
-  // }
+   onload(e) {
+     e.preventDefault();
+     const shorturl = {
+       shorturl: this.state.shorturl, 
+     };
+    this.props.giveshorturl(shorturl);
+   }
   render() {
-  console.log(shorturl);
-   
     return (
       <div>
         <div id="fullurl"></div>
         <div>"Wait for right url"</div>
-        <div onLoad={giveshorturl(shorturl)}></div>
       </div>
     );
   }
